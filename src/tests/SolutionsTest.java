958f74443d3ff27a5409e21388cca4ebb3c27a1d
Mikkel <mikkel@mediagram.dk>
2017-12-22 17:52:52 +0100

    Test work

--- src/tests/SolutionsTest.java
+++ src/tests/SolutionsTest.java
@@ -14,19 +14,26 @@ class SolutionsTest {
         Grid g = new Grid();
 
         boolean tester = false;
+        int count = 0;
         int[][] array = s.beginner(g.getBoardGrid());
         int[] nums = {1, 2, 3, 4};
         int[] temp = new int[4];
 
-        // Check first row horizontal
-        for (int i = 0; i < 4; i++) {
-            temp[i] = array[0][i];
-        }
+        // Check all horizontal rows
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                temp[count] = array[i][j];
+                count++;
+            }
 
-        Arrays.sort(temp);
+            Arrays.sort(temp);
 
-        if (Arrays.equals(temp, nums)) {
-            tester = true;
+            if (Arrays.equals(temp, nums)) {
+                tester = true;
+            } else {
+                break;
+            }
+
         }
 
         Assertions.assertTrue(tester);
